8b3cbd3292a38b4d783ad4a2a27ece6d286c27a4
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-01-05 22:40:22 +0200

    added active status

--- fetch-photo/src/components/Pagenator/Pagenator.js
+++ fetch-photo/src/components/Pagenator/Pagenator.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Pagenator.module.scss';
 
 
 
 function Pagenator(props) {
+
+  
 
   let hits = props.hits; 
   let per_page = props.per_pages;
@@ -12,19 +14,55 @@ function Pagenator(props) {
   arr.length = pages;
 
   for(let i = 0; i < arr.length; i++) {
-    arr[i] = i+1;
+
+    arr[i] = {
+      value: i+1,
+      isActive: false
+    }
+   
+    if (i === 0) {
+      arr[i] = {
+        value: i+1,
+        isActive: true
+      }
+    }
+
   }
 
-  const setPager = (i) => {
-    props.setPager(i);
+  const [allPages, setAllPages] = useState(arr);
+
+
+  const setPager = (index) => {
+    props.setPager(index);
+
+    setAllPages(prevVal => {
+
+      for(let i = 0; i < arr.length; i++) {
+
+        arr[i] = {
+          value: i+1,
+          isActive: false
+        }
+       
+        if (i === index) {
+          arr[i] = {
+            value: i+1,
+            isActive: true
+          }
+        }
+    
+      }
+
+      return arr;
+    });
   }
 
   return (
     <ul className={styles.Pagenator}>
-      {arr.map((item, index) => (
-              <li key={index} onClick={() => setPager(index)}>
-                {item}
-              </li>
+      {allPages.map((item, index) => (
+        <li key={index} onClick={() => setPager(index)} className={ item.isActive ? styles.active : '' }>
+          {item.value}
+        </li>
       ))}
     </ul>
   );
